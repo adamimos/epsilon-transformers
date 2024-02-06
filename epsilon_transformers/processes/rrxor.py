5b31e1cc05426f45b5da06e5a0e2b46a2994bfb8
melembroucarlitos <melembroucarlitos@gmail.com>
2024-02-06 04:29:29 +0000

    deleting uncessary function

--- epsilon_transformers/processes/rrxor.py
+++ epsilon_transformers/processes/rrxor.py
@@ -36,35 +36,3 @@ class RRXOR(Process):
 
         return T, state_names
 
-    def generate_without_epsilon_machine(self, total_length: int, with_positions=False):
-        """
-        Generate a sequence of Random-Random-XOR (RRXOR) data.
-
-        Parameters:
-        total_length (int): The total length of the sequence to generate.
-        with_positions (bool): If True, also return a list of positions ("R1", "R2", "XOR").
-
-        Returns:
-        list: The generated RRXOR sequence. If with_positions is True, also return a list of positions.
-        """
-        output = []
-        positions = []
-        
-        while len(output) < total_length + 3:
-            bit1 = random.randint(0, 1)
-            bit2 = random.randint(0, 1)
-            xor_result = bit1 ^ bit2
-            output.extend([str(bit1), str(bit2), str(xor_result)])
-            positions.extend(["R1", "R2", "XOR"])
-        
-        # Start the sequence randomly at bit 1,2 r 3
-        start_index = random.randint(0, 2)
-        output = output[start_index:]
-        positions = positions[start_index:]
-
-        # Return the sequence up to the desired total length along with positions
-        if with_positions:
-            return output[:total_length], positions[:total_length]
-        else:
-            return output[:total_length]
-
