c227f183fb59644225404b4c29c4d7283534faaa
melembroucarlitos <melembroucarlitos@gmail.com>
2024-02-13 22:29:14 +0000

    tests for generating single sequence & multiple sequences

--- tests/test_processes.py
+++ tests/test_processes.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pytest
 
+from epsilon_transformers.processes.process import ProcessHistory
 from epsilon_transformers.processes.zero_one_random import ZeroOneR
 
-# TODO: Check for off by 1 error in the asserts
-# TODO: Check that histogram distribution matches steady state distribution
+# TODO: Check for off by 1 error in the sample_emission asserts
+# TODO: Check that histogram distribution matches steady state distribution in test_generate_single_sequence as well as multpiple_sequences
+
 # TODO: parameterize this for each process
 # TODO: parameterize current_state_index
+# TODO: Parameterize number of sequences & sequence lengths
 
 def test_sample_emission():
     process = ZeroOneR()
@@ -20,3 +23,19 @@ def test_sample_emission():
     current_state_index = 2  # Replace with a valid index
     emission = process._sample_emission(current_state_index)
     assert 0 <= emission <= process.vocab_len
+
+def test_generate_single_sequence():
+    process = ZeroOneR()
+
+    outs = process.generate_single_sequence(12)
+    assert len(outs) == 12
+    assert isinstance(outs, ProcessHistory)
+
+def test_generate_multiple_sequences():
+    process = ZeroOneR()
+
+    outs = process.generate_multiple_sequences(12, 18)
+    assert len(outs) == 12
+    for seq in outs:
+        assert len(seq) == 18
+        assert isinstance(seq, ProcessHistory)
