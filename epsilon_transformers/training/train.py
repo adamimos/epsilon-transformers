f25141808bd958abf17142e0755878932c9aa66e
melembroucarlitos <melembroucarlitos@gmail.com>
2024-02-16 01:09:33 +0000

    _check_if_action_batch & correctind device passing & transformer_lens loss

--- epsilon_transformers/training/train.py
+++ epsilon_transformers/training/train.py
@@ -6,6 +6,9 @@ import torch
 
 from epsilon_transformers.training.configs import TrainConfig
 
+# TODO: Generalize train_model so that it doesn't depend on the HookedTransformer internal loss function
+# TODO: move _check_if_action_batch asserts to a config validator
+
 # TODO: Add eval part of the code
 # TODO: Couple eval, logging & saving frequencies (??)
 # TODO: Test _check_if_action_batch()
@@ -22,10 +25,14 @@ def _set_random_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-def _check_if_action_batch(action_frequency: int, total_tokens: int, batch_size: int, batch_idx: int) -> bool:
-    total_batches = total_tokens // batch_size
-    action_interval = total_batches // action_frequency
-    return (batch_idx + 1) % action_interval
+def _check_if_action_batch(perform_action_every_n_tokens: int, total_tokens: int, batch_size: int, sequence_len: int, batch_idx: int) -> bool:
+
+    tokens_per_batch = (batch_size * sequence_len)
+    assert perform_action_every_n_tokens > tokens_per_batch, "perform_action_every_n_tokens must be greater than tokens_per_batch"
+    total_batches = total_tokens // tokens_per_batch
+    perform_action_every_n_batches = perform_action_every_n_tokens // tokens_per_batch
+    action_interval = total_batches // perform_action_every_n_batches
+    return (batch_idx + 1) % action_interval == 0
 
 def _main(config_path: pathlib.Path):
     config: TrainConfig = TrainConfig.from_yaml(config_path)
@@ -35,29 +42,24 @@ def train_model(config: TrainConfig):
     device = torch.device("mps" if torch.backends.mps.is_available() else ("cuda" if torch.cuda.is_available() else "cpu"))
 
     _set_random_seed(config.seed)
-    criterion = torch.nn.CrossEntropyLoss(reduction='none')
 
     model = config.model.to_hooked_transformer(device=device, seed=config.seed)
     optimizer = config.optimizer.from_model(model=model, device=device)
-    dataloader = config.dataset.to_dataloader(sequence_length=model.cfg.n_ctx, device=device)
+    dataloader = config.dataset.to_dataloader(sequence_length=model.cfg.n_ctx)
     
     model.train()
-    total_loss = 0.0
     for batch_idx, (input_data, target_data) in enumerate(dataloader):
-        input_data, target_data = input_data.to(model.device), target_data.to(model.device)
+        input_data, target_data = input_data.to(device), target_data.to(device)
 
-        predictions = model(input_data)
-        loss = criterion(predictions, target_data)
+        loss = model(input_data, return_type="loss")
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        total_loss += loss.item()
-
         # TODO: Logging
         
-        if _check_if_action_batch(action_frequency=config.persistance.checkpoint_every_n_tokens, total_tokens=config.dataset.num_tokens, batch_size=config.dataset.batch_size, batch_idx=batch_idx):
+        if _check_if_action_batch(perform_action_every_n_tokens=config.persistance.checkpoint_every_n_tokens, total_tokens=config.dataset.num_tokens, batch_size=config.dataset.batch_size, batch_idx=batch_idx, sequence_len=config.model.n_ctx):
             config.persistance.save_model()
 
 
