5f6161f7340e60e7c6dbcdb502cce4aec8ee71a7
melembroucarlitos <melembroucarlitos@gmail.com>
2024-02-16 21:49:46 +0000

    eval scaffolding

--- epsilon_transformers/training/train.py
+++ epsilon_transformers/training/train.py
@@ -3,20 +3,17 @@ import pathlib
 import random
 import numpy as np
 import torch
+from transformer_lens import HookedTransformer
 
-from epsilon_transformers.training.configs import TrainConfig
+from epsilon_transformers.training.configs import TrainConfig, ProcessDatasetConfig, PersistanceConfig
 
+# TODO: Add TQDM to all of this
 # TODO: Generalize train_model so that it doesn't depend on the HookedTransformer internal loss function
 # TODO: move _check_if_action_batch asserts to a config validator
 
-# TODO: Add eval part of the code
-# TODO: Couple eval, logging & saving frequencies (??)
-# TODO: Test _check_if_action_batch()
 # TODO: Review best practices regarding seed setting
 # TODO: Add Wandb Logging
 # TODO: Test on GPUs
-# TODO: Implement to_hooked_transformer()
-# TODO: Implement save_model()
 # TODO: Add DP
 
 def _set_random_seed(seed):
@@ -31,21 +28,34 @@ def _check_if_action_batch(perform_action_every_n_tokens: int, batch_size: int, 
     perform_action_every_n_batches = perform_action_every_n_tokens // tokens_per_batch
     return (batch_idx + 1) % perform_action_every_n_batches == 0
 
+def _evaluate_model(model, eval_dataloader, eval_config, logging_config):
+    with torch.no_grad():
+        raise NotImplementedError
+def _log():
+    raise NotImplementedError
+
+def _evaluate_log_and_persist(dataset_config: ProcessDatasetConfig, logging_config, persistance_config: PersistanceConfig, model: HookedTransformer):
+    eval_dataloader = dataset_config.to_dataloader(sequence_length=model.cfg.n_ctx, train=False)
+    metrics = _evaluate_model(model, eval_dataloader, logging_config)
+    _log(metrics)
+    persistance_config.save_model()
+    return metrics
+
 def _main(config_path: pathlib.Path):
     config: TrainConfig = TrainConfig.from_yaml(config_path)
     train_model(config)
 
-def train_model(config: TrainConfig):
+def train_model(config: TrainConfig) -> HookedTransformer:
     device = torch.device("mps" if torch.backends.mps.is_available() else ("cuda" if torch.cuda.is_available() else "cpu"))
 
     _set_random_seed(config.seed)
 
     model = config.model.to_hooked_transformer(device=device, seed=config.seed)
     optimizer = config.optimizer.from_model(model=model, device=device)
-    dataloader = config.dataset.to_dataloader(sequence_length=model.cfg.n_ctx)
-    
+    train_dataloader = config.dataset.to_dataloader(sequence_length=model.cfg.n_ctx, train=True)
+
     model.train()
-    for batch_idx, (input_data, target_data) in enumerate(dataloader):
+    for batch_idx, (input_data, target_data) in enumerate(train_dataloader):
         input_data, target_data = input_data.to(device), target_data.to(device)
 
         loss = model(input_data, return_type="loss")
@@ -54,11 +64,14 @@ def train_model(config: TrainConfig):
         loss.backward()
         optimizer.step()
 
-        # TODO: Logging
-        
         if _check_if_action_batch(perform_action_every_n_tokens=config.persistance.checkpoint_every_n_tokens, batch_size=config.dataset.batch_size, batch_idx=batch_idx, sequence_len=config.model.n_ctx):
-            config.persistance.save_model()
-
+            model.eval()
+            _evaluate_log_and_persist(dataset_config=config.dataset, logging_config={}, persistance_config=config.persistance, model=model)
+            model.train()
+  
+    model.eval()
+    metrics = _evaluate_log_and_persist(dataset_config=config.dataset, logging_config={}, persistance_config=config.persistance, model=model)
+    return model, metrics
 
 if __name__ == "__main__":
     fire.Fire(_main)
