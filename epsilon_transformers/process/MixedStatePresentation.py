3b621ef666c8bc5e24bc0305e5a89b3d93b0259a
melembroucarlitos <melembroucarlitos@gmail.com>
2024-02-20 06:36:34 -0800

    init function for MixedStaetTreeNode

--- epsilon_transformers/process/MixedStatePresentation.py
+++ epsilon_transformers/process/MixedStatePresentation.py
@@ -1,12 +1,17 @@
 from jaxtyping import Float
 from typing import Set
 import numpy as np
-from uuid import UUID
+from uuid import UUID, uuid1
 
 class MixedStateTreeNode:
 	id: UUID
 	state_vector: Float[np.ndarray, "n_states"] # Prob vector
 	children: Set['MixedStateTreeNode']
+	
+	def __init__(self, state_vector: Float[np.ndarray, "n_states"], children: Set['MixedStateTreeNode']):
+		self.id = uuid1()
+		self.state_vector = state_vector
+		self.children = children
 	
 class MixedStateTree:
 	root_node: UUID
