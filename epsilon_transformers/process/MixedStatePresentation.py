e44ca0be7c7594204b768d7fe71198f16cf26c18
melembroucarlitos <melembroucarlitos@gmail.com>
2024-02-20 07:34:25 -0800

    removed ids, added MixedStateTree init

--- epsilon_transformers/process/MixedStatePresentation.py
+++ epsilon_transformers/process/MixedStatePresentation.py
@@ -1,19 +1,24 @@
 from jaxtyping import Float
 from typing import Set
 import numpy as np
-from uuid import UUID, uuid1
 
 class MixedStateTreeNode:
-	id: UUID
 	state_vector: Float[np.ndarray, "n_states"] # Prob vector
 	children: Set['MixedStateTreeNode']
 	
 	def __init__(self, state_vector: Float[np.ndarray, "n_states"], children: Set['MixedStateTreeNode']):
-		self.id = uuid1()
 		self.state_vector = state_vector
 		self.children = children
-	
+		
+	def add_child(self, child: 'MixedStateTreeNode'):
+		self.children.add(child)
+
 class MixedStateTree:
-	root_node: UUID
+	root_node: MixedStateTreeNode
 	process: str
 	nodes: Set[MixedStateTreeNode]
+	
+	def __init__(self, root_node: MixedStateTreeNode, process: str, nodes: Set[MixedStateTreeNode]):
+		self.root_node = root_node
+		self.process = process
+		self.nodes = nodes
