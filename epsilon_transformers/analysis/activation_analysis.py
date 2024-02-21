a689ef1b192811db9df1105ac55ed13accec3e41
melembroucarlitos <melembroucarlitos@gmail.com>
2024-02-21 14:13:46 -0800

    scaffolding todos for find_mps_simplex_in_residual_stream

---
+++ epsilon_transformers/analysis/activation_analysis.py
@@ -0,0 +1,12 @@
+from transformer_lens import HookedTransformer
+
+from epsilon_transformers.process.Process import Process
+
+def find_msp_simplex_in_residual_stream(model: HookedTransformer, process: Process):
+    # Create the Mixed State Presentation Tree
+    # Create the dataloader
+    # Derive the mixed_state belief vector 
+    # Save out the activations
+    # Run regression
+    # Reshape results into expected shape
+    raise NotImplementedError
