9182739456f4227d6dac92670cbc8b30b63a7ebd
melembroucarlitos <melembroucarlitos@gmail.com>
2024-03-24 22:25:37 +0000

    WIP refactoring tests for persister

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -1,13 +1,93 @@
+import pathlib
+import tempfile
 import boto3
+import pytest
 import torch
 from io import BytesIO
 from dotenv import load_dotenv
 
-# TODO: Implement this into the actual code base
+from epsilon_transformers.persistence import LocalPersister, S3Persister
+
 # TODO: Create a PIBBSS S3 account
 # TODO: Get Adam to move the relevant models into the new S3 account and buckets
-# TODO: Write up the ReadMe stuff
 # TODO: E2E training persistence example
+
+def load_local_model():
+    # Define a simple neural network
+    class SimpleNN(torch.nn.Module):
+        def __init__(self):
+            super(SimpleNN, self).__init__()
+            self.fc = torch.nn.Linear(10, 1)
+
+        def forward(self, x):
+            return self.fc(x)
+
+    # Create an instance of the neural network
+    model = SimpleNN()
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        model_filepath = pathlib.Path(temp_dir) / "model.pt"
+        torch.save(model.state_dict(), model_filepath)
+
+        persister = LocalPersister(collection_location=pathlib.Path(temp_dir))
+        loaded_model = persister.load_model(model=SimpleNN(), object_name="model.pt")
+
+    assert torch.all(torch.eq(loaded_model.state_dict()['fc.weight'], model.state_dict()['fc.weight']))
+    assert torch.all(torch.eq(loaded_model.state_dict()['fc.bias'], model.state_dict()['fc.bias']))
+
+def save_local_model():
+    raise NotImplementedError
+
+def save_and_load_s3_model():
+    bucket_name = 'lucas-new-test-bucket-003'
+    with pytest.raises(ValueError):
+        S3Persister(collection_location=bucket_name)
+    
+    # Create mock bucket
+    s3 = boto3.client('s3')
+    s3.create_bucket(Bucket=bucket_name)
+    
+    persister = S3Persister(collection_location=bucket_name)
+
+    # mock nn
+    # TODO: Mock needs to be randomly initialized
+    class SimpleNN(torch.nn.Module):
+        def __init__(self):
+            super(SimpleNN, self).__init__()
+            self.fc = torch.nn.Linear(10, 1)
+
+        def forward(self, x):
+            return self.fc(x)
+
+    model = SimpleNN()
+    
+    # test save
+    persister.save_model(model, 85)
+
+    download_buffer = BytesIO()
+    s3.download_fileobj(bucket_name, 'model.pt', download_buffer, 85)
+
+    # Load the downloaded network
+    downloaded_model = SimpleNN()
+    download_buffer.seek(0)  # Reset download buffer position to the beginning
+    downloaded_model.load_state_dict(torch.load(download_buffer))
+
+    # Assert that the downloaded network is the same as the original one
+    assert torch.all(torch.eq(model.state_dict()['fc.weight'], downloaded_model.state_dict()['fc.weight']))
+    assert torch.all(torch.eq(model.state_dict()['fc.bias'], downloaded_model.state_dict()['fc.bias']))
+
+    # Test save overwrite protection
+    with pytest.raises(ValueError):
+        persister.save_model(model, 85)
+
+    # Test load
+    loaded_model = persister.load_model(SimpleNN(), "85.pt")
+    assert torch.all(torch.eq(model.state_dict()['fc.weight'], loaded_model.state_dict()['fc.weight']))
+    assert torch.all(torch.eq(model.state_dict()['fc.bias'], loaded_model.state_dict()['fc.bias']))
+
+    # Delete mock bucket
+    s3.delete_object(Bucket=bucket_name, Key='85.pt')
+    s3.delete_bucket(Bucket=bucket_name)
 
 def test_s3_persistence_put_and_retrieve_object_from_bucket():
     # Define a simple neural network
@@ -98,4 +178,4 @@ def test_s3_create_and_delete_bucket():
     assert bucket_name not in bucket_names, f"Bucket {bucket_name} was not deleted"
 
 if __name__ == "__main__":
-    test_s3_create_and_delete_bucket()
+    save_and_load_s3_model()
