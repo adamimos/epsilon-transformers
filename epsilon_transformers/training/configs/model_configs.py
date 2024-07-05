42e078342901558e18e51bf417c915e94a87f055
Mateusz <mateusz.piotrowski94@gmail.com>
2024-07-05 19:01:23 +0000

    attn only

--- epsilon_transformers/training/configs/model_configs.py
+++ epsilon_transformers/training/configs/model_configs.py
@@ -13,6 +13,7 @@ class RawModelConfig(Config):
     n_head: int
     d_mlp: int
     n_layers: int
+    attn_only: bool = False
 
     def to_hooked_transformer(
         self, device: torch.device, seed: Optional[int] = None
@@ -25,6 +26,7 @@ class RawModelConfig(Config):
             n_heads=self.n_head,
             d_mlp=self.d_mlp,
             d_vocab=self.d_vocab,
+            attn_only=self.attn_only,
             seed=seed,
             device=device,
             act_fn="relu",
